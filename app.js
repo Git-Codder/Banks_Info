bc91a9571b941888dfad009d957a2abbdb143e4c
Aditya <kumawataditya1358@gmail.com>
2021-05-23 23:53:42 +0530

    add corse policy

--- app.js
+++ app.js
@@ -13,6 +13,27 @@ var bodyParser = require('body-parser'),
     app.use(bodyParser.urlencoded({
         extended: true
     }))
+
+
+    app.use(function (req, res, next) {
+        /*var err = new Error('Not Found');
+         err.status = 404;
+         next(err);*/
+      
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+      
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+      
+      //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      
+        // Pass to next layer of middleware
+        next();
+    });
     
     app.use('/', express.static(path.join(__dirname, '/')))
     app.use('/views', express.static(path.join(__dirname, '/views')))
